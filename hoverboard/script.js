6ae29097eb6ce1a8119a42b1672237ce55b756bb
student-PiyushMishra <student.piyushmishra@gmail.com>
2024-11-24 20:32:31 +0530

    Day 7 - Included the sound feature in Project 6

--- hoverboard/script.js
+++ hoverboard/script.js
@@ -1,27 +1,31 @@
 const hoverboard = document.querySelector(".hoverboard");
 const boxes = document.querySelectorAll(".box");
 const audio = new Audio;
-audio.src = "assets/stop-13692.mp3"
 
-hoverboard
-.addEventListener("mouseover",(event)=>{
-    if(event.target.classList == "box"){
-        let color = `rgba(${generateRandomColor()},${generateRandomColor()},${generateRandomColor()},1)`
-        event.target.style.backgroundColor = color;
-        event.target.style.boxShadow = `0px 0px 10px ${color}`;
-        audio.play();
-    }
+document.querySelector("button").addEventListener("click", function() {
+    this.style.opacity = 0;
+    audio.src = "assets/stop-13692.mp3";
 })
 
-boxes.forEach(elem=>{
-    elem.addEventListener("mouseleave",()=>{
+hoverboard
+    .addEventListener("mouseover", (event) => {
+        if (event.target.classList == "box") {
+            let color = `rgba(${generateRandomColor()},${generateRandomColor()},${generateRandomColor()},1)`
+            event.target.style.backgroundColor = color;
+            event.target.style.boxShadow = `0px 0px 10px ${color}`;
+            audio.play();
+        }
+    })
+
+boxes.forEach(elem => {
+    elem.addEventListener("mouseleave", () => {
         setTimeout(() => {
             elem.style.backgroundColor = `rgb(42, 41, 41)`;
-            elem.style.boxShadow = `0px 0px 2px rgba(255, 255, 255, 0.262)`; 
+            elem.style.boxShadow = `0px 0px 2px rgba(255, 255, 255, 0.262)`;
         }, 1000);
     })
 })
 
-const generateRandomColor = ()=>{
+const generateRandomColor = () => {
     return Math.floor(Math.random() * 256);
 }
