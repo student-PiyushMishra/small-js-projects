94919aa35176a2207963fabd6569acd6896dc898
student-PiyushMishra <student.piyushmishra@gmail.com>
2024-11-24 20:12:55 +0530

    Day 7 - Project 6 Completed

---
+++ hoverboard/script.js
@@ -0,0 +1,27 @@
+const hoverboard = document.querySelector(".hoverboard");
+const boxes = document.querySelectorAll(".box");
+const audio = new Audio;
+audio.src = "assets/stop-13692.mp3"
+
+hoverboard
+.addEventListener("mouseover",(event)=>{
+    if(event.target.classList == "box"){
+        let color = `rgba(${generateRandomColor()},${generateRandomColor()},${generateRandomColor()},1)`
+        event.target.style.backgroundColor = color;
+        event.target.style.boxShadow = `0px 0px 10px ${color}`;
+        audio.play();
+    }
+})
+
+boxes.forEach(elem=>{
+    elem.addEventListener("mouseleave",()=>{
+        setTimeout(() => {
+            elem.style.backgroundColor = `rgb(42, 41, 41)`;
+            elem.style.boxShadow = `0px 0px 2px rgba(255, 255, 255, 0.262)`; 
+        }, 1000);
+    })
+})
+
+const generateRandomColor = ()=>{
+    return Math.floor(Math.random() * 256);
+}
