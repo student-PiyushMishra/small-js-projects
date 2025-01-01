252c2afe4e6c2176cfdcdd84ddf41dd98b305638
student-PiyushMishra <student.piyushmishra@gmail.com>
2025-01-01 15:37:36 +0530

    made the frontend for the quiz-app, added some dummy questions and created the rendering logic and navigation throughout questions

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,1 +1,63 @@
- 
+let questionDiv = document.querySelector(".question");
+let optionsArr = document.querySelectorAll(".option");
+let nextQue = document.querySelector(".right");
+let backQue = document.querySelector(".left");
+let data = [];
+let askedQuestions = [];
+let questionNum = 0;
+
+function fetchData() {
+    return fetch('questions.json')
+        .then(response => response.json())
+        .then(JSONdata => {
+            data = JSONdata;
+            return JSONdata;
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+}
+async function fetchingData() {
+    await fetchData();
+}
+
+const loggingQuestions = async () => {
+    creatingQuestions();
+    if (questionNum < askedQuestions.length) {
+        let question = askedQuestions[questionNum];
+        questionDiv.textContent = `${questionNum + 1}. ${question.question}`;
+        optionsArr.forEach((val, index) => {
+            val.querySelector('.txt').textContent = question.options[index];
+        });
+        questionNum++;
+    }
+}
+
+const creatingQuestions = () => {
+    if (data.length === 0) {
+        return;
+    }
+    let question = data[Math.floor(Math.random() * data.length)];
+    data.splice(data.indexOf(question), 1);
+    askedQuestions.push(question);
+    return;
+}
+
+function reverseQuestion() {
+    if (questionNum > 0) {
+        questionNum--;
+        let question = askedQuestions[questionNum];
+        questionDiv.textContent = `${questionNum + 1}. ${question.question}`;
+        optionsArr.forEach((val, index) => {
+            val.querySelector('.txt').textContent = question.options[index];
+        });
+    }
+}
+
+
+const app = async () => {
+    await fetchingData();
+    window.addEventListener("load", loggingQuestions);
+    nextQue.addEventListener('click', loggingQuestions);
+    backQue.addEventListener('click', reverseQuestion);
+}
+
+app();
